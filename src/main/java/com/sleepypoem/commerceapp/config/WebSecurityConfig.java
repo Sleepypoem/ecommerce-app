19680adb5c13ab8d0ebfad397023e807edfce585
Alexander <gallade2014@gmail.com>
2023-01-02 08:28:37 -0600

    added security configuration and secured actuators.

---
+++ src/main/java/com/sleepypoem/commerceapp/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.sleepypoem.commerceapp.config;
+
+import com.sleepypoem.commerceapp.config.security.JwtAuthConverter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class WebSecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/actuator/health").permitAll()
+                .requestMatchers("/actuator/metrics").authenticated()
+                .requestMatchers("/actuator/loggers").authenticated()
+                .requestMatchers("/api/*").authenticated()
+                .anyRequest().authenticated());
+        http.oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthConverter);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors().and().csrf().disable();
+        return http.build();
+    }
+}
