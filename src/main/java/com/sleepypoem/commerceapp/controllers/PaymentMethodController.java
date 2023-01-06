8e66b344a07b8a2c957ec9be3ba0b0fc63a8afeb
Alexander <gallade2014@gmail.com>
2023-01-05 22:13:34 -0600

    added payment method service and controller

---
+++ src/main/java/com/sleepypoem/commerceapp/controllers/PaymentMethodController.java
@@ -0,0 +1,4 @@
+package com.sleepypoem.commerceapp.controllers;
+
+public class PaymentMethodController {
+}
---
+++ src/main/java/com/sleepypoem/commerceapp/services/PaymentMethodService.java
@@ -0,0 +1,42 @@
+package com.sleepypoem.commerceapp.services;
+
+import com.sleepypoem.commerceapp.domain.dto.PaymentMethodDto;
+import com.sleepypoem.commerceapp.domain.entities.PaymentMethodEntity;
+import com.sleepypoem.commerceapp.domain.mappers.BaseMapper;
+import com.sleepypoem.commerceapp.domain.mappers.PaymentMethodMapper;
+import com.sleepypoem.commerceapp.repositories.PaymentMethodRepository;
+import com.sleepypoem.commerceapp.services.abstracts.AbstractService;
+import com.sleepypoem.commerceapp.services.validators.IValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentMethodService extends AbstractService<PaymentMethodDto, PaymentMethodEntity> {
+
+    final static IValidator<PaymentMethodEntity> VALIDATE_PAYMENT_METHOD = paymentMethod -> {
+        return true;
+    };
+
+    @Autowired
+    PaymentMethodRepository dao;
+
+    @Autowired
+    PaymentMethodMapper mapper;
+    @Override
+    protected JpaRepository<PaymentMethodEntity, Long> getDao() {
+        return dao;
+    }
+
+    @Override
+    protected BaseMapper<PaymentMethodEntity, PaymentMethodDto> getMapper() {
+        return mapper;
+    }
+
+    @Override
+    protected IValidator<PaymentMethodEntity> getValidator() {
+        return null;
+    }
+
+
+}
