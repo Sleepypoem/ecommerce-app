8c011e3c96f1cfb62781b13cf0f2133be91d0a2e
Alexander <gallade2014@gmail.com>
2023-01-11 07:58:36 -0600

    added payment not supported exception

---
+++ src/main/java/com/sleepypoem/commerceapp/exceptions/MyUnsupportedPaymentMethodException.java
@@ -0,0 +1,16 @@
+package com.sleepypoem.commerceapp.exceptions;
+
+public class MyUnsupportedPaymentMethodException extends Exception{
+
+    public MyUnsupportedPaymentMethodException(String message) {
+        super(message);
+    }
+
+    public MyUnsupportedPaymentMethodException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyUnsupportedPaymentMethodException(Throwable cause) {
+        super(cause);
+    }
+}
