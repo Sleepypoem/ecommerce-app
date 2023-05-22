336d1c8084293ed6ac14bb897cc4c6f69bc70594
Alexander <gallade2014@gmail.com>
2023-05-22 14:00:24 -0600

    Added data.sql to populate the db.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,41 @@
+INSERT INTO `ecommerce`.`addresses`
+(`id`,
+`created_at`,
+`updated_at`,
+`country`,
+`first_line`,
+`second_line`,
+`state`,
+`user_id`,
+`zip_code`)
+VALUES
+(1,
+'2008-11-11 13:23:44',
+'2008-11-11 13:23:44',
+"El Salvador",
+"dadadada",
+"",
+"Santa Ana",
+"365d1c65-8a79-436f-88dc-8c471f64558a",
+0000);
+
+INSERT INTO `ecommerce`.`addresses`
+(`id`,
+`created_at`,
+`updated_at`,
+`country`,
+`first_line`,
+`second_line`,
+`state`,
+`user_id`,
+`zip_code`)
+VALUES
+(2,
+'2008-12-11 13:23:44',
+'2012-11-11 13:23:44',
+"El Salvador",
+"dadadada2",
+"",
+"San Miguel",
+"365d1c65-8a79-436f-88dc-8c471f64558a",
+0000);
