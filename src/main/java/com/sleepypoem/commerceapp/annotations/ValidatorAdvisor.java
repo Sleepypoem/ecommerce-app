ef35aedc9eb9ace2325157c9f9263d3986bba844
Alexander <gallade2014@gmail.com>
2023-05-24 00:20:41 -0600

    Extracted the validation logic to an Aspect, added Validable and ValidableMethod annotations and an advisor to validate entities.

---
+++ src/main/java/com/sleepypoem/commerceapp/annotations/ValidatorAdvisor.java
@@ -0,0 +1,76 @@
+package com.sleepypoem.commerceapp.annotations;
+
+import com.sleepypoem.commerceapp.domain.abstracts.AbstractEntity;
+import com.sleepypoem.commerceapp.exceptions.MyValidableAnnotationException;
+import com.sleepypoem.commerceapp.exceptions.MyValidationException;
+import com.sleepypoem.commerceapp.services.validators.IValidator;
+import com.sleepypoem.commerceapp.services.validators.Validator;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Type;
+
+@Aspect
+@Component
+@Slf4j
+public class ValidatorAdvisor {
+
+    @Before("validableMethodPointcut() && validableMethodAnnotationPointcut()")
+    public void validateMethod(JoinPoint joinPoint) {
+        IValidator<?> validator = extractValidatorClass(joinPoint.getTarget().getClass());
+        AbstractEntity<?> entity = extractEntityFromArgs(joinPoint.getArgs());
+        @SuppressWarnings({"rawtypes","unchecked"})
+        Validator validatorClass = new Validator( validator, entity);
+        try{
+            validatorClass.validate();
+        }catch (ClassCastException e){
+            throw new MyValidationException("Error in validator, expected validator for: " + entity.getClass().getSimpleName()
+                    + " in Validable annotation in  class: " + joinPoint.getTarget().getClass().getSimpleName()
+                    , e);
+        }
+
+    }
+
+    @Pointcut("execution( public * (@com.sleepypoem.commerceapp.annotations.Validable *)+.*(..))")
+    public void validableMethodPointcut() {}
+
+    @Pointcut("@annotation(com.sleepypoem.commerceapp.annotations.ValidableMethod)")
+    public void validableMethodAnnotationPointcut() {}
+
+    private IValidator<?> extractValidatorClass(Class<?> clazz) {
+        if (clazz == null) {
+            throw new IllegalArgumentException("Class cannot be null.");
+        }
+
+        Validable annotation = clazz.getAnnotation(Validable.class);
+
+        Class<? extends IValidator<?>> validatorClass = annotation.value();
+        try {
+            Constructor<? extends IValidator<?>> constructor = validatorClass.getDeclaredConstructor();
+            return constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            throw new MyValidableAnnotationException("Failed to instantiate validator.", e);
+        }
+    }
+
+    private AbstractEntity<?> extractEntityFromArgs(Object[] args) {
+        AbstractEntity<?> entity = null;
+        for (Object arg : args) {
+            if (arg instanceof AbstractEntity<?>) {
+                entity = (AbstractEntity<?>) arg;
+            }
+        }
+        if (entity == null) {
+            throw new MyValidableAnnotationException("Annotated method must have an argument of type AbstractEntity");
+        }
+        return entity;
+    }
+
+}
