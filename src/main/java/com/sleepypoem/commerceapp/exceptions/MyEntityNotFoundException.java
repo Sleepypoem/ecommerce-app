7326ebc80c8b359e36a62c1382ed395e2b3fed04
Alexander <gallade2014@gmail.com>
2023-06-04 22:35:15 -0600

    Corrected response status of MyEntityNotFoundException and added a handler for the not found exception.

--- src/main/java/com/sleepypoem/commerceapp/exceptions/MyEntityNotFoundException.java
+++ src/main/java/com/sleepypoem/commerceapp/exceptions/MyEntityNotFoundException.java
@@ -3,7 +3,7 @@ package com.sleepypoem.commerceapp.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public final class MyEntityNotFoundException extends RuntimeException {
 
     public MyEntityNotFoundException() {
