dd280a36c37990883e6a3bf1133ce827bbd74ca9
Alexander <gallade2014@gmail.com>
2023-06-12 15:06:16 -0600

    Removed paginator's tests because I have to think again in unit tests, I will add them ASAP.

--- src/test/java/com/sleepypoem/commerceapp/controllers/utils/PaginatorTest.java
+++ src/test/java/com/sleepypoem/commerceapp/controllers/utils/PaginatorTest.java
@@ -1,52 +1,5 @@
 package com.sleepypoem.commerceapp.controllers.utils;
 
-import com.sleepypoem.commerceapp.domain.dto.PaginatedDto;
-import com.sleepypoem.commerceapp.domain.dto.ProductDto;
-import com.sleepypoem.commerceapp.domain.entities.ProductEntity;
-import com.sleepypoem.commerceapp.domain.mappers.BaseMapper;
-import com.sleepypoem.commerceapp.domain.mappers.ProductMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class PaginatorTest {
-
-    Paginator<ProductEntity, ProductDto> paginator;
-
-    BaseMapper<ProductEntity, ProductDto> mapper;
-
-    @BeforeEach
-    void setUp() {
-        mapper = new ProductMapper();
-        paginator = new Paginator<>(mapper);
-    }
-
-    @Test
-    void testGetPaginatedDto() {
-        //arrange
-        List<ProductEntity> entities = List.of(
-                ProductEntity.builder().id(1L).name("testProduct").price(100.0).stock(100).build()
-        );
-        Page<ProductEntity> pagedEntity = new PageImpl<>(entities);
-
-        //act
-        PaginatedDto<ProductDto> paginatedDto = paginator.getPaginatedDto(pagedEntity, "products");
-
-        //assert
-        assertAll(
-                () -> assertEquals(0, paginatedDto.getCurrentPage()),
-                () -> assertEquals(1, paginatedDto.getTotalElements()),
-                () -> assertEquals(1, paginatedDto.getContent().size()),
-                () -> assertNull(paginatedDto.getPreviousPage()),
-                () -> assertEquals("testProduct", paginatedDto.getContent().get(0).getName()),
-                () -> assertEquals(100.0, paginatedDto.getContent().get(0).getPrice()),
-                () -> assertEquals(100, paginatedDto.getContent().get(0).getStock())
-        );
-    }
 
 }
