78cd28702d9a1c696794473800b00d5d06de6c4a
Alexander <gallade2014@gmail.com>
2023-06-12 12:29:40 -0600

    Modified MyBadRequestException to add a keycloak error dto if the error comes from the auth server.

--- src/main/java/com/sleepypoem/commerceapp/exceptions/MyBadRequestException.java
+++ src/main/java/com/sleepypoem/commerceapp/exceptions/MyBadRequestException.java
@@ -1,21 +1,30 @@
 package com.sleepypoem.commerceapp.exceptions;
 
+import com.sleepypoem.commerceapp.domain.dto.KeycloakErrorDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public final class MyBadRequestException extends RuntimeException {
 
-    public MyBadRequestException(final String message, final Throwable cause) {
-        super(message, cause);
+    private KeycloakErrorDto errorDto;
+
+    public MyBadRequestException(final String message, KeycloakErrorDto errorDto) {
+        super(message);
+        this.errorDto = errorDto;
     }
 
     public MyBadRequestException(final String message) {
         super(message);
     }
 
-    public MyBadRequestException(final Throwable cause) {
+    public MyBadRequestException(final Throwable cause, KeycloakErrorDto errorDto) {
         super(cause);
+        this.errorDto = errorDto;
+    }
+
+    public KeycloakErrorDto getErrorDto() {
+        return errorDto;
     }
 
 }
