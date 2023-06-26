f8f14565de6a404807ab6c55f9ba7ee6c56a58ac
Alexander <gallade2014@gmail.com>
2023-06-26 12:00:48 -0600

    Modified some method names to match other services method names.

--- src/main/java/com/sleepypoem/commerceapp/services/UserService.java
+++ src/main/java/com/sleepypoem/commerceapp/services/UserService.java
@@ -1,11 +1,12 @@
 package com.sleepypoem.commerceapp.services;
 
 import com.sleepypoem.commerceapp.domain.dto.UserDto;
-import com.sleepypoem.commerceapp.domain.dto.UserRepresentationDto;
 import com.sleepypoem.commerceapp.exceptions.MyUserNotFoundException;
 import com.sleepypoem.commerceapp.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -17,24 +18,28 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public UserDto getUserByUserName(String userName) throws MyUserNotFoundException {
+    public UserDto getOneByUserName(String userName) throws MyUserNotFoundException {
         return userRepository.getUserByUserName(userName);
     }
 
-    public UserDto getUserById(String id) throws MyUserNotFoundException {
+    public UserDto getOneById(String id) throws MyUserNotFoundException {
         return userRepository.getUserById(id);
     }
 
-    public String addUser(UserRepresentationDto user) {
+    public String create(UserDto user) {
         return userRepository.create(user).getId();
     }
 
-    public void deleteUser(String id) {
-        userRepository.deleteUser(id);
+    public boolean delete(String id) {
+        return userRepository.deleteUser(id);
     }
 
-    public UserDto updateUser(String id, UserRepresentationDto user) throws MyUserNotFoundException {
+    public UserDto update(String id, UserDto user) throws MyUserNotFoundException {
         return userRepository.update(id, user);
+    }
+
+    public List<UserDto> getAllByCriteria(String criteriaName, String criteriaValue, Integer firstResult, Integer maxResults, boolean exact) {
+        return userRepository.getAllUsersByCriteria(criteriaName, criteriaValue, firstResult, maxResults, exact);
     }
 
 }
