d24e27f6756aa9a351b55c4dae1c3f0cf7e57f4a
Alexander <gallade2014@gmail.com>
2023-06-26 10:58:50 -0600

    Modified signature of some methods based on fixes that I found while testing, also fixed minor bugs and modified some exceptions.

--- src/main/java/com/sleepypoem/commerceapp/config/keycloak/KeyCloakFacadeImpl.java
+++ src/main/java/com/sleepypoem/commerceapp/config/keycloak/KeyCloakFacadeImpl.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sleepypoem.commerceapp.config.beans.AuthServerPropertyLoader;
 import com.sleepypoem.commerceapp.domain.dto.KeycloakErrorDto;
 import com.sleepypoem.commerceapp.domain.dto.UserDto;
-import com.sleepypoem.commerceapp.domain.dto.UserRepresentationDto;
+import com.sleepypoem.commerceapp.exceptions.MyBadRequestException;
 import com.sleepypoem.commerceapp.exceptions.MyInternalException;
 import com.sleepypoem.commerceapp.exceptions.MyUserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 @Component
 @Slf4j
@@ -27,64 +28,81 @@ private static final String USERS_ENDPOINT = "/users/";
 
     private final String realmName;
 
-    private final KeycloakAuthorizerImpl keycloakAuthorizerImpl;
+    private final KeyCloakAuthorizer keyCloakAuthorizer;
 
-    public KeyCloakFacadeImpl(ObjectMapper mapper, AuthServerPropertyLoader authServerPropertyLoader, KeycloakAuthorizerImpl keycloakAuthorizerImpl) {
+    private final RequestHelper requestHelper;
+
+    public KeyCloakFacadeImpl(ObjectMapper mapper,
+                              AuthServerPropertyLoader authServerPropertyLoader,
+                              KeyCloakAuthorizer keyCloakAuthorizer,
+                              RequestHelper requestHelper) {
         this.mapper = mapper;
         this.realmName = authServerPropertyLoader.getRealmName();
         this.adminRestPrefix = authServerPropertyLoader.getAdminRestPrefix();
-        this.keycloakAuthorizerImpl = keycloakAuthorizerImpl;
+        this.keyCloakAuthorizer = keyCloakAuthorizer;
+        this.requestHelper = requestHelper;
     }
 
     @Override
-    public UserDto updateUser(String userId, UserRepresentationDto userRepresentationDto) {
+    public UserDto updateUser(String userId, UserDto userDto) {
         log.info("Attempting to update user with id: " + userId);
-        HttpHeaders headers = RequestCommons.createHeaders(MediaType.APPLICATION_JSON, keycloakAuthorizerImpl.getAccessToken());
+        HttpHeaders headers = requestHelper.createHeaders(MediaType.APPLICATION_JSON, keyCloakAuthorizer.getAccessToken());
 
-        HttpEntity<String> request = new HttpEntity<>(userRepresentationDto.toJsonString(), headers);
-        RequestCommons.makeRequest(adminRestPrefix + realmName + USERS_ENDPOINT + userId, HttpMethod.PUT, request);
-        return getUserById(userId);
+        HttpEntity<String> request = requestHelper.createHttpEntity(userDto.toJsonString(), headers);
+        requestHelper.makeRequest(adminRestPrefix + realmName + USERS_ENDPOINT + userId, HttpMethod.PUT, request);
+        return userDto;
     }
 
 
     @Override
-    public void deleteUser(String id) {
-        log.info("Attempting to delete user with id: " + id);
-        HttpHeaders headers = RequestCommons.createHeaders(MediaType.APPLICATION_JSON, keycloakAuthorizerImpl.getAccessToken());
+    public boolean deleteUser(String id) {
+        log.info("Attempting to deleteById user with id: " + id);
+        HttpHeaders headers = requestHelper.createHeaders(MediaType.APPLICATION_JSON, keyCloakAuthorizer.getAccessToken());
 
-        RequestCommons.makeRequest(adminRestPrefix + realmName + USERS_ENDPOINT + id,
-                HttpMethod.DELETE,
-                RequestCommons.createEmptyHttpEntity(headers));
+        try {
+            requestHelper.makeRequest(adminRestPrefix + realmName + USERS_ENDPOINT + id,
+                    HttpMethod.DELETE,
+                    requestHelper.createEmptyHttpEntity(headers));
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public UserDto getUserByUserName(String username) {
         log.info("Attempting to retrieve user with username: " + username);
-        HttpHeaders headers = RequestCommons.createHeaders(MediaType.APPLICATION_JSON, keycloakAuthorizerImpl.getAccessToken());
+        HttpHeaders headers = requestHelper.createHeaders(MediaType.APPLICATION_JSON, keyCloakAuthorizer.getAccessToken());
 
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<String> response = RequestCommons.makeRequest(
+        HttpEntity<String> entity = requestHelper.createEmptyHttpEntity(headers);
+        ResponseEntity<String> response = requestHelper.makeRequest(
                 adminRestPrefix + realmName + "/users?username=" + username + "&exact=true", HttpMethod.GET, entity);
-        if ("[]".equals(response.getBody())) {
+        List<UserDto> foundUsers = mapToUserDtoList(response.getBody());
+        if (foundUsers.isEmpty()) {
             KeycloakErrorDto keycloakErrorDto = new KeycloakErrorDto();
             keycloakErrorDto.setError("User not found");
-            throw new MyUserNotFoundException("User with username: " + username + " not found", keycloakErrorDto);
+            throw new MyUserNotFoundException("User with username: " + username + ", not found", keycloakErrorDto);
         }
-        return mapToUserDtoList(response.getBody()).get(0);
+        return foundUsers.get(0);
     }
 
     @Override
-    public UserDto createUser(UserRepresentationDto user) {
+    public UserDto createUser(UserDto user) {
         log.info("Attempting to add user: " + user.getUsername());
-        HttpHeaders headers = RequestCommons.createHeaders(MediaType.APPLICATION_JSON, keycloakAuthorizerImpl.getAccessToken());
+        HttpHeaders headers = requestHelper.createHeaders(MediaType.APPLICATION_JSON, keyCloakAuthorizer.getAccessToken());
 
-        HttpEntity<String> request = new HttpEntity<>(user.toJsonString(), headers);
-        RequestCommons.makeRequest(adminRestPrefix + realmName + USERS_ENDPOINT, HttpMethod.POST, request);
+        HttpEntity<String> request = requestHelper.createHttpEntity(user.toJsonString(), headers);
+        requestHelper.makeRequest(adminRestPrefix + realmName + USERS_ENDPOINT, HttpMethod.POST, request);
 
-        return getUserByUserName(user.getUsername());
+        return user;
     }
 
     private List<UserDto> mapToUserDtoList(String response) {
+        if(response == null || response.equals("[]")) {
+            return Collections.emptyList();
+        }
+
         try {
             return mapper.readValue(response, mapper.getTypeFactory().constructCollectionType(List.class, UserDto.class));
         } catch (JsonProcessingException e) {
@@ -95,11 +113,11 @@ private static final String USERS_ENDPOINT = "/users/";
     @Override
     public UserDto getUserById(String id) {
         log.info("Attempting to retrieve user by id: " + id);
-        HttpHeaders headers = RequestCommons.createHeaders(MediaType.APPLICATION_JSON, keycloakAuthorizerImpl.getAccessToken());
+        HttpHeaders headers = requestHelper.createHeaders(MediaType.APPLICATION_JSON, keyCloakAuthorizer.getAccessToken());
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        ResponseEntity<String> response = RequestCommons.makeRequest(adminRestPrefix + realmName + USERS_ENDPOINT + id,
+        ResponseEntity<String> response = requestHelper.makeRequest(adminRestPrefix + realmName + USERS_ENDPOINT + id,
                 HttpMethod.GET,
-                RequestCommons.createEmptyHttpEntity(headers)
+                requestHelper.createEmptyHttpEntity(headers)
         );
 
         return UserDto.fromJsonString(response.getBody());
@@ -108,21 +126,37 @@ private static final String USERS_ENDPOINT = "/users/";
     @Override
     public List<UserDto> getAllUsersByCriteria(String criteriaName, String criteriaValue, Integer firstResult, Integer maxResults, boolean exact) {
         log.info("Attempting to retrieve all users by criteria: " + criteriaName + " with value: " + criteriaValue);
-        HttpHeaders headers = RequestCommons.createHeaders(MediaType.APPLICATION_JSON, keycloakAuthorizerImpl.getAccessToken());
-        HttpEntity<String> entity = RequestCommons.createEmptyHttpEntity(headers);
-        ResponseEntity<String> response = RequestCommons.makeRequest(createPaginatedRequest(criteriaName, criteriaValue, firstResult, maxResults, exact),
+        checkCriteriaNotNull(criteriaName, criteriaValue);
+        HttpHeaders headers = requestHelper.createHeaders(MediaType.APPLICATION_JSON, keyCloakAuthorizer.getAccessToken());
+        HttpEntity<String> entity = requestHelper.createEmptyHttpEntity(headers);
+        ResponseEntity<String> response = requestHelper.makeRequest(createPaginatedRequestUrl(criteriaName, criteriaValue, firstResult, maxResults, exact),
                 HttpMethod.GET, entity);
 
         return mapToUserDtoList(response.getBody());
 
     }
 
-    private String createPaginatedRequest(String criteriaName, String criteriaValue, Integer page, Integer size, Boolean exact) {
+    private void checkCriteriaNotNull(String criteriaName, String criteriaValue) {
+        try {
+            Objects.requireNonNull(criteriaName, "Criteria name cannot be null");
+            Objects.requireNonNull(criteriaValue, "Criteria value cannot be null");
+        } catch (NullPointerException e) {
+            throw new MyBadRequestException(e.getMessage());
+        }
+    }
+
+    private String createPaginatedRequestUrl(String criteriaName, String criteriaValue, Integer page, Integer size, Boolean exact) {
         return adminRestPrefix + realmName + "/users?" + criteriaName + "=" + criteriaValue + "&first=" + page + "&max=" + size + "&exact=" + exact;
     }
 
     @Override
     public Long countUsersByCriteria(String criteriaName, String criteriaValue) {
-        return null;
+        log.info("Attempting to count users by criteria: " + criteriaName + " with value: " + criteriaValue);
+        checkCriteriaNotNull(criteriaName, criteriaValue);
+        HttpHeaders headers = requestHelper.createHeaders(MediaType.APPLICATION_JSON, keyCloakAuthorizer.getAccessToken());
+        HttpEntity<String> entity = requestHelper.createEmptyHttpEntity(headers);
+        ResponseEntity<String> response = requestHelper.makeRequest(adminRestPrefix + realmName + "/users/count?" + criteriaName + "=" + criteriaValue,
+                HttpMethod.GET, entity);
+        return response.getBody() == null ? 0L : Long.parseLong(response.getBody());
     }
 }
