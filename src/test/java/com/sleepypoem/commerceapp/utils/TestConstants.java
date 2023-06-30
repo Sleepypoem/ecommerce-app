d5bf51a082bde41754134c18dcc405e3ed58e587
Alexander <gallade2014@gmail.com>
2023-06-29 21:36:18 -0600

    Modified pageable to test first and last page cases.

--- src/test/java/com/sleepypoem/commerceapp/utils/TestConstants.java
+++ src/test/java/com/sleepypoem/commerceapp/utils/TestConstants.java
@@ -9,7 +9,9 @@ public class TestConstants {
     private TestConstants() {
     }
 
-    public static final int DEFAULT_PAGE = 0;
+    public static final int DEFAULT_FIRST_PAGE = 0;
+
+    public static final int DEFAULT_LAST_PAGE = 4;
 
     public static final int DEFAULT_SIZE = 10;
 
@@ -21,5 +23,7 @@ public class TestConstants {
 
     public static final Long ZERO_TOTAL_ELEMENTS = 0L;
 
-    public static final Pageable DEFAULT_PAGEABLE = PageRequest.of(DEFAULT_PAGE, DEFAULT_SIZE, Sort.by(DEFAULT_SORT_BY).ascending());
+    public static final Pageable DEFAULT_PAGEABLE_AT_FIRST_PAGE = PageRequest.of(DEFAULT_FIRST_PAGE, DEFAULT_SIZE, Sort.by(DEFAULT_SORT_BY).ascending());
+
+    public static final Pageable DEFAULT_PAGEABLE_AT_LAST_PAGE = PageRequest.of(DEFAULT_LAST_PAGE, DEFAULT_SIZE, Sort.by(DEFAULT_SORT_BY).ascending());
 }
