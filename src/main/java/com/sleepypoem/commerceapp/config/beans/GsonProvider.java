703e91240d71f726217ac52e5c8212259786f7a7
Alexander <gallade2014@gmail.com>
2023-06-29 21:55:49 -0600

    Modified the Gson Constructor to add builders params if needed.

--- src/main/java/com/sleepypoem/commerceapp/config/beans/GsonProvider.java
+++ src/main/java/com/sleepypoem/commerceapp/config/beans/GsonProvider.java
@@ -1,13 +1,15 @@
 package com.sleepypoem.commerceapp.config.beans;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class GsonProvider {
 
     private GsonProvider() {
     }
 
-    private static final Gson gson = new Gson();
+    private static final Gson gson = new GsonBuilder()
+            .create();
 
     public static Gson getGson() {
         return gson;
